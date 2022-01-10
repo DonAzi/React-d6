3f68f05fbc24eeda6606670db68053c4341fd2c3
DonAzi <savaSh-00@mail.ru>
2022-01-10 15:18:58 +0000

    Update CommentArea.jsx

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,30 +1,33 @@
-import { Component } from "react";
-import { Form, Col } from "react-bootstrap";
+import { Form, Col, Button } from "react-bootstrap";
+import React from "react";
 
-class CommentArea extends Component {
+class CommentArea extends React.Component {
   state = {
     comments: [], // we will receive comments to this array and pass to where necessary
   };
 
   componentDidMount = async () => {
+    console.log(JSON.stringify(this.props));
     try {
       let response = await fetch(
-        "https://striveschool-api.herokuapp.com/api/comments/",
+        "https://striveschool-api.herokuapp.com/api/comments/" +
+          this.props.elementId,
         {
           headers: {
             Authorization:
-              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIyMDAzOTY4YjNlMDAwMTViN2FjYjEiLCJpYXQiOjE2MzkwNTU0MTgsImV4cCI6MTY0MDI2NTAxOH0.XTaSP1lDIsQ_Ug_tZNsfvN_fuvKMP8Jx1AsQTiyG7VA" +
-              this.props.asin,
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIyMDAzOTY4YjNlMDAwMTViN2FjYjEiLCJpYXQiOjE2MzkwNTU0MTgsImV4cCI6MTY0MDI2NTAxOH0.XTaSP1lDIsQ_Ug_tZNsfvN_fuvKMP8Jx1AsQTiyG7VA",
           },
         }
       );
 
       let comments = await response.json();
-      this.state({ comments: comments });
+      this.setState({ comments: comments });
+      console.log(comments);
     } catch (error) {
       console.log(error);
     }
   };
+
   //rendering will be after fetch complete
   render() {
     return (
@@ -34,11 +37,43 @@ class CommentArea extends Component {
           <Col>
             <Form.Control type="text" placeholder="Comment" />
           </Col>
+          <Form.Label column lg={2}></Form.Label>
+          <Col>
+            {this.state.comments.map((com) => (
+              <div> {com.comment}</div>
+            ))}
+            <Button
+              onClick={() => {
+                postComment(this.props.elementId, "hi");
+              }}
+            >
+              submit
+            </Button>
+          </Col>
         </Form.Row>
         <br />
       </>
     );
   }
 }
+
+const postComment = async (asin, comment) => {
+  try {
+    await fetch("https://striveschool-api.herokuapp.com/api/comments/", {
+      method: "POST",
+      body: JSON.stringify({
+        elementId: asin,
+        rate: 1,
+        comment: comment,
+      }),
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIyMDAzOTY4YjNlMDAwMTViN2FjYjEiLCJpYXQiOjE2MzkwNTU0MTgsImV4cCI6MTY0MDI2NTAxOH0.XTaSP1lDIsQ_Ug_tZNsfvN_fuvKMP8Jx1AsQTiyG7VA",
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export default CommentArea;
